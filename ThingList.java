64e4f75ae8c1dadee321f4f122b793e4680ebb75
Vansh <vanshisawesomer@gmail.com>
2025-10-06 00:07:45 -0400

    added thinglist.java

---
+++ ThingList.java
@@ -0,0 +1,38 @@
+import java.util.Random;
+
+public class ThingList {
+    private Node head;
+
+    private class Node {
+        Thing data;
+        Node next;
+
+        Node(Thing t) {
+            data = t;
+            next = null;
+        }
+    }
+
+    public void addThing(Thing t) {
+        Node n = new Node(t);
+        n.next = head;
+        head = n;
+    }
+
+    public void moveAll(Random rand) {
+        Node cur = head;
+        while (cur != null) {
+            cur.data.maybeTurn(rand);
+            cur.data.step();
+            cur = cur.next;
+        }
+    }
+
+    public void printAll() {
+        Node cur = head;
+        while (cur != null) {
+            System.out.println(cur.data.lab + " at " + cur.data.getPosition());
+            cur = cur.next;
+        }
+    }
+}
